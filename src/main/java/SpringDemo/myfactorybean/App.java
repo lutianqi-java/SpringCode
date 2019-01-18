d5107f824b11eb85cab27bbdf475cc383b24e818
lutianqi <1179555783@qq.com>
2019-01-18 15:41:51 +0800

    增加日志

--- src/main/java/SpringDemo/myfactorybean/App.java
+++ src/main/java/SpringDemo/myfactorybean/App.java
@@ -1,24 +1,30 @@
 package SpringDemo.myfactorybean;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
+import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 
 public class App {
 
-    public static void main(String[] args) {
-        //实例化BeanFactory
-        Resource res = new ClassPathResource("applicationContext.xml");
-        BeanFactory bf = new XmlBeanFactory(res);
+    private static Logger logger = Logger.getLogger(App.class);
 
-        //获得bean
-//        PersonFactoryBean personFactory = bf.getBean("personFactory", PersonFactoryBean.class);
+    public static void main(String[] args) {
+//        //实例化BeanFactory
+//        Resource res = new ClassPathResource("applicationContext.xml");
+//        BeanFactory bf = new XmlBeanFactory(res);
+//
+//        //获得bean
+////        Person personFactory = bf.getBean("person", Person.class);
 //        PersonFactoryBean personFactory = bf.getBean("&personFactory", PersonFactoryBean.class);
-        Person personFactory = bf.getBean("personFactory", Person.class);
+////        Person personFactory = bf.getBean("personFactory", Person.class);
+//        System.out.println(personFactory);
+
+        ApplicationContext applicationContext = new FileSystemXmlApplicationContext("E:\\SpringCode\\src\\main\\resources\\applicationContext.xml");
+        Person personFactory = applicationContext.getBean("person", Person.class);
         System.out.println(personFactory);
 
-    }
+}
 
 }
 
