790adb3b02e2ad8af6bf6a170af35da91f9bb0bd
lutianqi <1179555783@qq.com>
2019-04-25 09:57:42 +0800

    增加日志

--- src/main/java/SpringDemo/myfactorybean/App.java
+++ src/main/java/SpringDemo/myfactorybean/App.java
@@ -1,8 +1,13 @@
 package SpringDemo.myfactorybean;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 
 
 public class App {
@@ -10,21 +15,22 @@ public class App {
     private static Logger logger = Logger.getLogger(App.class);
 
     public static void main(String[] args) {
-//        //实例化BeanFactory
-//        Resource res = new ClassPathResource("applicationContext.xml");
-//        BeanFactory bf = new XmlBeanFactory(res);
+        //实例化BeanFactory
+        Resource res = new ClassPathResource("applicationContext.xml");
+        Resource ress = new FileSystemResource("");
+        BeanFactory bf = new XmlBeanFactory(res);
 //
 //        //获得bean
-////        Person personFactory = bf.getBean("person", Person.class);
+        Person personFactory = bf.getBean("person", Person.class);
 //        PersonFactoryBean personFactory = bf.getBean("&personFactory", PersonFactoryBean.class);
 ////        Person personFactory = bf.getBean("personFactory", Person.class);
 //        System.out.println(personFactory);
-
+//
         ApplicationContext applicationContext = new FileSystemXmlApplicationContext("E:\\SpringCode\\src\\main\\resources\\applicationContext.xml");
-        Person personFactory = applicationContext.getBean("person", Person.class);
-        System.out.println(personFactory);
+//        Person personFactory = applicationContext.getBean("person", Person.class);
+//        System.out.println(personFactory);
 
-}
+    }
 
 }
 
