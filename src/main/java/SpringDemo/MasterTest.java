c1da7de43636e7f17b750d73448d4f7482cf3cc1
lutianqi <1179555783@qq.com>
2019-04-25 10:03:23 +0800

    dev分支

---
+++ src/main/java/SpringDemo/MasterTest.java
@@ -0,0 +1,15 @@
+package SpringDemo;
+
+/**
+ * @Description
+ * @Author 卢天琦
+ * @Date 2019/4/25  10:02
+ * @Version 1.0.0
+ **/
+public class MasterTest {
+
+
+    public static void main(String[] args){
+        System.out.println("Master分支");
+    }
+}
